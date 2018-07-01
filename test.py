9e717e6cc0812bb923cbe3b7e986dd0048809640
MohamedHeshamMustafa <mohamed.hesham.mostafa@gmail.com>
2018-07-01 03:32:09 +0200

    Test.py modification - Bug Fixed: Creating results folder

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ import time
 import os
 import sys
 import tensorflow as tf
-from tensorflow.contrib.data import Iterator
+from tensorflow.python.data import Iterator
 from util.data_loader import *
 from util.data_process import *
 from util.train_test_func import *
@@ -442,6 +442,16 @@ def test(config_file):
         test_time.append(time.time() - t0)
         final_label = np.zeros(temp_size, np.int16)
         final_label = set_ND_volume_roi_with_bounding_box_range(final_label, temp_bbox[0], temp_bbox[1], out_label)
+
+		# Check if results path exists. if not, create it
+        fullpath = save_folder+"/{0:}.nii.gz".format(temp_name) # get full path
+        basedir = os.path.dirname(fullpath)
+        if not os.path.exists(basedir): # if path doesn't exist
+        	os.makedirs(basedir) # create it
+
+        with open(fullpath, 'a'): # touch the file
+        	os.utime(fullpath, None)
+
         save_array_as_nifty_volume(final_label, save_folder+"/{0:}.nii.gz".format(temp_name), img_names[0])
         print(temp_name)
     test_time = np.asarray(test_time)
